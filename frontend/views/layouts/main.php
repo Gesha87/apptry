9c07ce1f3fef2955e44432474efd22457c8c9e59
Andrey Kulik <kulik.andrei87@gmail.com>
2015-03-17 13:59:47 +0600

    fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -54,7 +54,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Gesha <?= date('Y') ?></p>
         <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
