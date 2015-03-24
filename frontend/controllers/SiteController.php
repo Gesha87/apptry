702cb575497d186def823de23261b4b1b4439f36
Andrey Kulik <kulik.andrei87@gmail.com>
2015-03-24 13:49:41 +0600

    fix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -1,6 +1,8 @@
 <?php
 namespace frontend\controllers;
 
+use CFPropertyList\CFPropertyList;
+use Exception;
 use Yii;
 use yii\web\Controller;
 
@@ -21,5 +23,25 @@ class SiteController extends Controller
 	public function actionAbout()
 	{
 		return $this->render('about');
+	}
+
+	public function actionUdid()
+	{
+		$data = file_get_contents('php://input');
+		$xml = substr($data, strpos($data, '<plist '));
+		$xml = substr(substr($data, strpos($data, '<plist ')), 0, strpos($xml, '</plist>') + 8);
+		$this->redirect(['site/result', 'data' => $xml], 301);
+	}
+
+	public  function actionResult()
+	{
+		$properties = [];
+		$plist = new CFPropertyList();
+		try {
+			$plist->parse($_GET['data']);
+			$properties = $plist->toArray();
+		} catch (Exception $ex) {}
+
+		$this->render('result', ['properties' => $properties]);
 	}
 }
--- frontend/web/udid.php
+++
@@ -1,4 +0,0 @@
-<?php
-$data = file_get_contents('php://input');
-header('HTTP/1.1 301 Moved Permanently');
-header('Location: http://apptry.camlyapp.com/result/?data='.rawurldecode($data));
